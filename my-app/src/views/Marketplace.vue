f1b8cf54963905f0431efd40fa2828223136cfa6
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-04-03 23:24:21 -0400

    n

--- my-app/src/views/Marketplace.vue
+++ my-app/src/views/Marketplace.vue
@@ -1,29 +1,62 @@
 <script setup>
 import {v4 as uuidv4} from 'uuid';
 import { supabase } from '@/lib/supabaseClient.js'
-let myuuid = uuidv4();
-const now = new Date();
-const poop = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} EDT`
+import { ref } from 'vue'
 
-supabase
-  .from('userdata')
-  .insert([{ uuid: myuuid, created_at: poop, inventory: ['hi'] }])
-  
-  .then(response => {
-       console.log('UUID inserted successfully:', response);
-   })
-   .catch(error => {
-       console.error('Error inserting UUID:', error);
-   });
+let username = ref()
+let password = ref()
+function getPosition(e){
+  e.preventDefault();
+  let myuuid = uuidv4();
+  const now = new Date();
+  const poop = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} EDT`
+  supabase
+    .from('userdata')
+    .insert([{ uuid: myuuid, created_at: poop, inventory: ['hi'], username: username.value, password: password.value }])
+    
+    .then(response => {
+        console.log('UUID inserted successfully:', response);
+    })
+    .catch(error => {
+        console.error('Error inserting UUID:', error);
+    });
+}
+async function signUp(email, password) {
+    try {
+        const { user, session, error } = await supabase.auth.signUp({
+            email,
+            password,
+        });
+
+        if (error) {
+            throw error;
+        }
+
+        console.log('Sign-up successful:', user);
+    } catch (error) {
+        console.error('Sign-up error:', error.message);
+    }
+}
 
 </script>
 
 <template>
   <div class="about">
-    <h1>{{ myuuid }}</h1>
+    <form class="positionForm">
+      <label id="hi" for="hi">username</label>
+      <input v-model="username" required/>
+      <label id="hi" for="hello">password</label>
+      <input v-model="password" required/>
+      <input id="hi" type="submit" value="submit" @click="getPosition">
+    </form>
   </div>
 </template>
 
-<style>
-
+<style scoped>
+.positionForm{
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+  justify-content: center;
+}
 </style>
