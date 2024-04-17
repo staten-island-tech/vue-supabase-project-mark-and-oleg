674b7fe1c2f8c25b1cd1f5b81f990754b29ed38b
markk87 <markk87@nycstudents.net>
2024-04-17 09:19:41 -0400

    Update AuthUser.vue

--- my-app/src/components/AuthUser.vue
+++ my-app/src/components/AuthUser.vue
@@ -8,11 +8,23 @@ const email = ref('')
 const password = ref('')
 const username = ref('')
 
+// function PlaySound(melody) {
+//         alert("On Press of "+melody);
+//         var path = "path\\to\\melody\\"
+//         var snd = new Audio(path + melody + ".mp3");
+//         snd.play();
+//     }
+
+// function PlaySound() {
+//   const bflat = new Audio("@/public/skibidi.mp3");
+//   bflat.src = "bflat.mp3";
+//   document.getElementById(bflat);
+//   bflat.play();
+// }
+
 async function handleLogin() {
-  const audio = new Audio("@/public/skibidi.mp3");
-
-  audio.play();
-
+  
+  PlaySound();
   try {
 
     const userData = await supabase.auth.signUp({
@@ -38,6 +50,7 @@ async function handleLogin() {
 </script>
 
 <template>
+  <audio id="bflat"> </audio>
   <form class="row flex-center flex" @submit.prevent="handleLogin">
     <div class="col-6 form-widget">
       <h1 class="header">Supabase + Vue 3</h1>
