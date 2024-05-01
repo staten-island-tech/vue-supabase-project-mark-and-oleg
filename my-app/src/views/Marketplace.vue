bd4121a8f5de856ceed0c741116e860e63546233
markk87 <markk87@nycstudents.net>
2024-05-01 09:21:52 -0400

    Update Marketplace.vue

--- my-app/src/views/Marketplace.vue
+++ my-app/src/views/Marketplace.vue
@@ -4,15 +4,60 @@ import * as THREE from 'three';
 import { supabase } from '@/lib/supabaseClient.js'
 import { ref, onMounted } from 'vue'
 import { boxesList } from '@/stores/boxes.ts'
-let fartArr = []
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(-30, 0, 30);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.enableDamping = true;
+controls.update();
+
+
+
+
+const light = new THREE.AmbientLight(0x404040, 100);
+scene.add(light);
+
+const loader = new GLTFLoader();
+
+let model;
+
+loader.load(
+    './skibcrate.glb',
+    function (gltf) {
+        model = gltf.scene;
+        const bbox = new THREE.Box3().setFromObject(model);
+        const center = bbox.getCenter(new THREE.Vector3());
+        model.position.sub(center);
+        scene.add(model);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
+
 
 function felch(x){
   setTimeout(()=> fanum(x), 2500)
