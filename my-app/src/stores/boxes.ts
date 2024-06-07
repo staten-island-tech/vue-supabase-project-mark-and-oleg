c4024215ce3ef0f488ab622b33d58530ecbaa010
markk87 <markk87@nycstudents.net>
2024-06-06 22:12:53 -0400

    Update boxes.ts

--- my-app/src/stores/boxes.ts
+++ my-app/src/stores/boxes.ts
@@ -40,21 +40,21 @@ export const boxesList = ref([
                 itemType: 'skibidi',
                 itemrarity: 'common',
                 probability: .25,
-                imageLink: 'https://static.wikia.nocookie.net/dom-studios-skibidi-multiverse/images/4/44/DualBuzzsawKnives022.png/revision/latest?cb=20240128092115'
+                imageLink: 'https://preview.redd.it/skibidi-toilets-got-astro-technology-v0-pjz51cykgyuc1.jpeg?auto=webp&s=183bffed9c5c2f0b43111ebb663c7c3af7842a81'
             },
             {
                 item: 'Regular Speakerman',
                 itemType: 'skibidi',
                 itemrarity: 'common',
                 probability: .25,
-                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/9/9c/Skibidi_toilet_speakermanpng.png/revision/latest?cb=20230720114727'
+                imageLink: 'https://preview.redd.it/speakerman-theory-how-they-know-whats-there-v0-c464nodg2zqc1.png?width=205&format=png&auto=webp&s=4bce51bab65a35c645521c97fe4ca6824a2eaf3c'
             },
             {
                 item: 'Regular TV Man',
                 itemType: 'skibidi',
                 itemrarity: 'common',
                 probability: .25,
-                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/e/e3/Skibidi_toilet_39_Tv_Man_Screenshot_Hd_Enchanted.png/revision/latest/scale-to-width-down/1200?cb=20230617101724'
+                imageLink: 'https://ih1.redbubble.net/image.5221738827.4425/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
             },
         ],
         itemType: 'crate'
@@ -75,55 +75,64 @@ export const boxesList = ref([
                 item: 'Police Skibidi Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/9/9e/Newimgmediumpolice.png/revision/latest/scale-to-width-down/1200?cb=20231112052744'
             },
             {
                 item: 'Military Skibidi Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://media.sketchfab.com/models/55d48703e9424b03a90ba43eafe055d2/thumbnails/213540d40bc14197be337c644879c96c/3ab630f5f7a4406b88704f3423cc6899.jpeg'
             },
             {
                 item: 'Spider Skibidi Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/e/e0/StriderSkibidi.png/revision/latest/scale-to-width-down/1200?cb=20240202025626'
             },
             {
                 item: 'Zombie Skibidi Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-zombie-universe/images/f/f0/2024-03-03_18-22-50-666.png/revision/latest?cb=20240303114617'
             },
             {
                 item: 'Spider Speakerman',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/3/30/Better_SpeakerStrider.png/revision/latest/scale-to-width-down/1200?cb=20230721105829'
             },
             {
                 item: 'Recorder Cameraman',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://i.pinimg.com/originals/53/06/5e/53065e2e5240095da232cab91ddbb0ea.png'
             },
             {
                 item: 'Giant Cameraman',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/6/6c/34-LargeRedSuitedCameramanTransparent.png/revision/latest?cb=20230930193614'
             },
             {
-                item: 'Spider Cameraman',
+                item: 'Opium Spider Cameraman',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/0/0b/32-BetterCameraStrider.png/revision/latest/scale-to-width-down/1200?cb=20230721105826'
             },
             {
                 item: 'Spy Cameraman Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'uncommon',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/3/3b/Better_CameraToilet.png/revision/latest?cb=20230721105830'
             },
         ],
         itemType: 'crate'
@@ -137,7 +146,8 @@ export const boxesList = ref([
                 item: 'Holy Skibidi Toilet',
                 itemType: 'skibidi',
                 itemrarity: 'rare',
-                probability: .25
+                probability: .25,
+                imageLink: 'https://static.wikia.nocookie.net/skibidi-toilet-official/images/b/bb/10-SaintSkbidi.png/revision/latest?cb=20240312053622'
             },
             {
                 item: 'Hydra Skibidi Toilet',
@@ -254,13 +264,13 @@ export const boxesList = ref([
             {
                 item: 'Advisor Astro Toilet',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'legendary',
                 probability: .25
             },
             {
                 item: 'Cinemaman',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'legendary',
                 probability: .25
             },
             {
@@ -280,25 +290,25 @@ export const boxesList = ref([
             {
                 item: 'Final Boss Skibidi Toilet',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'Exotic',
                 probability: .25
             },
             {
                 item: 'Titan Speakerman',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'Exotic',
                 probability: .25
             },
             {
                 item: 'Titan Cameraman',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'Exotic',
                 probability: .25
             },
             {
                 item: 'Titan TV Man',
                 itemType: 'skibidi',
-                itemrarity: 'c',
+                itemrarity: 'Exotic',
                 probability: .25
             },
         ],
