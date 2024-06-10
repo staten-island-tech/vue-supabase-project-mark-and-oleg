2b997b0129e6e2997893b53190999815fa338522
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-06-09 23:06:26 -0400

    Update vite.config.ts

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -6,11 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['three/examples/jsm/loaders/GLTFLoader.js']
-    }
-  },
+
   plugins: [
     vue(),
     VueDevTools(),
